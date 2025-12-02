7099245f5f1db3e3b4842d418e8f0a610ee9279a
dhakeddevendra5 <dhakeddevendra5.com>
2025-12-02 21:56:57 +0530

    initial landing page test

---
+++ src/app/(HomePage)/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/header";
+import LandingHero from "@/components/Hero/LandingHero";
+import Footer from "@/components/Footer/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <LandingHero />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Hero/LandingHero.tsx
@@ -0,0 +1,17 @@
+export default function LandingHero() {
+  return (
+    <section className="px-6 py-24 text-center max-w-4xl mx-auto">
+      <h1 className="text-5xl font-bold mb-6">
+        Build Real-Time Video, Audio, and Chat
+      </h1>
+
+      <p className="text-xl text-gray-600 mb-10">
+        WebRTC + WebSockets powered communication directly in your browser.
+      </p>
+
+      <button className="bg-black text-white px-8 py-3 rounded-lg text-lg hover:bg-gray-900 transition">
+        Get Started
+      </button>
+    </section>
+  );
+}
